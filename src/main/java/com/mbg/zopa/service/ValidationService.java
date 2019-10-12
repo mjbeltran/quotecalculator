3b00a58e1e30b5d73a269ac84014eb46d7139406
Manuel <mjbeltrangarcia@gmail.com>
2019-10-12 14:49:37 +0200

    commit 2 con push

--- src/main/java/com/mbg/zopa/service/ValidationService.java
+++ src/main/java/com/mbg/zopa/service/ValidationService.java
@@ -12,4 +12,6 @@ public interface ValidationService {
 	void validateFileName(String filename);
 
 	void validateMarketHasMoneyForLoan(Integer requestedAmount, long availableLendersAmount);
+	
+	//TESTTTTTYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
 }
